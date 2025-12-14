e899ebe791aacfe606a05652e86656cd0815160c
Sammy Albataineh <sammyalbataineh@gmail.com>
2025-12-13 20:40:23 -0500

    Made a premier league match predictor

---
+++ premierLeague.py
@@ -0,0 +1,67 @@
+import pandas as pd
+df23 = pd.read_csv("E0 (2).csv")
+df24 = pd.read_csv("E0 (1).csv")
+df25 = pd.read_csv("E0.csv")
+df = pd.concat([df23, df24, df25], ignore_index=True)
+df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
+df = df.sort_values("Date").reset_index(drop=True)
+train_df = df[df["Date"].dt.year <= 2024]
+test_df  = df[df["Date"].dt.year == 2025]
+teams = pd.concat([df["HomeTeam"], df["AwayTeam"]]).unique()
+ratings = {team: 0.0 for team in teams}
+BASE_MULT = 1.2
+WIN_BONUS = 10
+AWAY_BONUS = 8
+GOAL_WEIGHT = 1.0
+MIN_MULT = 1.0
+REFERENCE_YEAR = 2025
+REFERENCE_MONTH = 12
+def recency_multiplier(match_date):
+    months_ago = (REFERENCE_YEAR - match_date.year) * 12 + (REFERENCE_MONTH - match_date.month)
+    mult = BASE_MULT - (months_ago / 10) - (months_ago / 12)
+    return max(MIN_MULT, mult)
+def add_val(goals_scored, won, away, date):
+    val = goals_scored * GOAL_WEIGHT
+    if won:
+        val += WIN_BONUS
+        if away:
+            val += AWAY_BONUS
+    mult = recency_multiplier(date)
+    return val * mult
+for _, row in train_df.iterrows():
+    home_team = row["HomeTeam"]
+    away_team = row["AwayTeam"]
+    home_goals = row["FTHG"]
+    away_goals = row["FTAG"]
+    home_win = home_goals > away_goals
+    away_win = away_goals > home_goals
+    date = row["Date"]
+    ratings[home_team] += add_val(home_goals, home_win, False, date)
+    ratings[away_team] += add_val(away_goals, away_win, away_win, date)
+total = 0
+correct = 0
+for _, row in test_df.iterrows():
+    home = row["HomeTeam"]
+    away = row["AwayTeam"]
+    prediction = home if ratings[home] * BASE_MULT >= ratings[away] else away
+    if row["FTHG"] != row["FTAG"]:
+        actual = home if row["FTHG"] > row["FTAG"] else away
+        if prediction == actual:
+            correct += 1
+        total += 1
+    home_goals = row["FTHG"]
+    away_goals = row["FTAG"]
+    home_win = home_goals > away_goals
+    away_win = away_goals > home_goals
+    date = row["Date"]
+    ratings[home] += add_val(home_goals, home_win, False, date)
+    ratings[away] += add_val(away_goals, away_win, away_win, date)
+accuracy = (correct / total) * 100
+print(f"Accuracy (no draws): {accuracy:.2f}%")
+def predict(home,away):
+    prediction = home if ratings[home] * BASE_MULT >= ratings[away] else away
+    print(f"{prediction} will beat {away if away != prediction else home}")
+predict("Crystal Palace","Man City")
+predict("Sunderland","Newcastle")
+predict("West Ham","Aston Villa")
+predict("Nott'm Forest","Tottenham")
